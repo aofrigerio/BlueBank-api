ccb6da07223a4296e0f6135d557561b7004f1223
Alan Frigério <alanfrigerio@finchsolucoes.com.br>
2021-10-30 18:00:38 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/codemasters/bluebank/domain/entities/ClientEntity.java
@@ -0,0 +1,29 @@
+package br.com.codemasters.bluebank.domain.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientEntity {
+
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    private String telephoneNumber;
+
+
+}
