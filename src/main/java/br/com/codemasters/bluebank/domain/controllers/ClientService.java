a4574f315f97019e7d6c003dced0fe29e7febb8f
VIVIANI ANDRADE MACHADO <9001010@riachuelo.com.br>
2021-11-04 16:58:11 -0300

    CRUD de clientes

---
+++ src/main/java/br/com/codemasters/bluebank/domain/controllers/ClientService.java
@@ -0,0 +1,47 @@
+package br.com.codemasters.bluebank.domain.controllers;
+
+import br.com.codemasters.bluebank.domain.entities.ClientEntity;
+import br.com.codemasters.bluebank.domain.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("clients")
+public class ClientService {
+    private final ClientRepository clientRepository;
+    @Autowired
+    public ClientService(ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getClients(){
+        return new ResponseEntity<>(clientRepository.findAll(), HttpStatus.valueOf(200));
+    }
+
+    @GetMapping(path = "{/id}")
+    public ResponseEntity<?> getClientById(@PathVariable("id") Long id){
+       if(clientRepository.existsById(id))
+         return new ResponseEntity<>(clientRepository.findById(id), HttpStatus.valueOf(200));
+       return new ResponseEntity<>(HttpStatus.valueOf(404));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveClient(@RequestBody ClientEntity clientEntity){
+        return new ResponseEntity<>(clientRepository.save(clientEntity), HttpStatus.valueOf(200));
+    }
+
+    @DeleteMapping(path ="{/id}")
+    public ResponseEntity<?> deleteClient(@PathVariable("id") Long id){
+        clientRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.valueOf(200));
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateClient(@RequestBody ClientEntity clientEntity){
+        return new ResponseEntity<>(clientRepository.save(clientEntity), HttpStatus.valueOf(200));
+    }
+
+}
--- src/main/java/br/com/codemasters/bluebank/domain/repository/ClientRepository.java
+++ src/main/java/br/com/codemasters/bluebank/domain/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package br.com.codemasters.bluebank.domain.repository;
 
 import br.com.codemasters.bluebank.domain.entities.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<ClientEntity, Long> {
 }
