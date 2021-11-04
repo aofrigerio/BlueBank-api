359772fc9d416fd7dc55907d85f56e7c37875401
VIVIANI ANDRADE MACHADO <9001010@riachuelo.com.br>
2021-11-04 20:36:38 -0300

    alteração no código dos endpoints

--- src/main/java/br/com/codemasters/bluebank/domain/controllers/ClientController.java
+++ src/main/java/br/com/codemasters/bluebank/domain/controllers/ClientController.java
@@ -3,14 +3,11 @@ package br.com.codemasters.bluebank.domain.controllers;
 import br.com.codemasters.bluebank.domain.entities.ClientEntity;
 import br.com.codemasters.bluebank.domain.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
-@RequestMapping("/clients")
+@RequestMapping({"/clients"})
 public class ClientController {
     private final ClientRepository clientRepository;
 
@@ -20,29 +17,45 @@ public class ClientController {
     }
 
     @GetMapping
-    public ResponseEntity<?> getClients(){
+    public ResponseEntity getClients(){
         return ResponseEntity.ok(clientRepository.findAll());
     }
 
-    @GetMapping(path = "{/id}")
-    public ResponseEntity<?> getClientById(@PathVariable("id") Long id){
-        return ResponseEntity.ok(clientRepository.findById(id));
+    @GetMapping(path = "/{id}")
+    public ResponseEntity getClientById(@PathVariable("id") Long id){
+        return clientRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<?> saveClient(@RequestBody ClientEntity clientEntity){
+    public ResponseEntity saveClient(@RequestBody ClientEntity clientEntity){
         return ResponseEntity.ok(clientRepository.save(clientEntity));
     }
 
-    @DeleteMapping(path ="{/id}")
+    @DeleteMapping(path ="/{id}")
     public ResponseEntity<?> deleteClient(@PathVariable("id") Long id){
-        clientRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
+        return clientRepository.findById(id)
+                .map(record -> {
+                    clientRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 
-    @PutMapping
-    public ResponseEntity<?> updateClient(@RequestBody ClientEntity clientEntity){
-        return ResponseEntity.ok(clientRepository.save(clientEntity));
+    @PutMapping(value="/{id}")
+    public ResponseEntity updateClient(@PathVariable("id") long id, @RequestBody ClientEntity clientEntity){
+        return clientRepository.findById(id)
+                .map(record -> {
+                    record.setName(clientEntity.getName());
+                    record.setAdress(clientEntity.getAdress());
+                    record.setCpf(clientEntity.getCpf());
+                    record.setRg(clientEntity.getRg());
+                    record.setSex(clientEntity.getSex());
+                    record.setEmail(clientEntity.getEmail());
+                    record.setTelephoneNumber(clientEntity.getTelephoneNumber());
+                    ClientEntity updated = clientRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
     }
 
 }
