73b21723a31905abccb65794dea460c909a4f10e
VIVIANI ANDRADE MACHADO <9001010@riachuelo.com.br>
2021-11-05 09:38:49 -0300

    criação de service ClientService

--- src/main/java/br/com/codemasters/bluebank/domain/controllers/ClientController.java
+++ src/main/java/br/com/codemasters/bluebank/domain/controllers/ClientController.java
@@ -1,61 +1,41 @@
 package br.com.codemasters.bluebank.domain.controllers;
 
 import br.com.codemasters.bluebank.domain.entities.ClientEntity;
-import br.com.codemasters.bluebank.domain.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import br.com.codemasters.bluebank.services.ClientService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping({"/clients"})
 public class ClientController {
-    private final ClientRepository clientRepository;
 
-    @Autowired
-    public ClientController(ClientRepository clientRepository){
-        this.clientRepository = clientRepository;
-    }
+    private final ClientService clientService;
 
     @GetMapping
     public ResponseEntity getClients(){
-        return ResponseEntity.ok(clientRepository.findAll());
+        return clientService.getClients();
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity getClientById(@PathVariable("id") Long id){
-        return clientRepository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
+        return clientService.getClientById(id);
     }
 
     @PostMapping
     public ResponseEntity saveClient(@RequestBody ClientEntity clientEntity){
-        return ResponseEntity.ok(clientRepository.save(clientEntity));
+        return clientService.saveClient(clientEntity);
     }
 
     @DeleteMapping(path ="/{id}")
     public ResponseEntity<?> deleteClient(@PathVariable("id") Long id){
-        return clientRepository.findById(id)
-                .map(record -> {
-                    clientRepository.deleteById(id);
-                    return ResponseEntity.ok().build();
-                }).orElse(ResponseEntity.notFound().build());
+        return clientService.deleteClient(id);
     }
 
     @PutMapping(value="/{id}")
     public ResponseEntity updateClient(@PathVariable("id") long id, @RequestBody ClientEntity clientEntity){
-        return clientRepository.findById(id)
-                .map(record -> {
-                    record.setName(clientEntity.getName());
-                    record.setAdress(clientEntity.getAdress());
-                    record.setCpf(clientEntity.getCpf());
-                    record.setRg(clientEntity.getRg());
-                    record.setSex(clientEntity.getSex());
-                    record.setEmail(clientEntity.getEmail());
-                    record.setTelephoneNumber(clientEntity.getTelephoneNumber());
-                    ClientEntity updated = clientRepository.save(record);
-                    return ResponseEntity.ok().body(updated);
-                }).orElse(ResponseEntity.notFound().build());
+        return clientService.updateClient(id, clientEntity);
     }
 
 }
---
+++ src/main/java/br/com/codemasters/bluebank/services/ClientService.java
@@ -0,0 +1,54 @@
+package br.com.codemasters.bluebank.services;
+
+import br.com.codemasters.bluebank.domain.entities.ClientEntity;
+import br.com.codemasters.bluebank.domain.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientRepository){
+        this.clientRepository = clientRepository;
+    }
+
+    public ResponseEntity getClients(){
+        return ResponseEntity.ok(clientRepository.findAll());
+    }
+
+    public ResponseEntity getClientById(Long id){
+        return clientRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity saveClient(ClientEntity clientEntity){
+        return ResponseEntity.ok(clientRepository.save(clientEntity));
+    }
+
+    public ResponseEntity<?> deleteClient(Long id){
+        return clientRepository.findById(id)
+                .map(record -> {
+                    clientRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    public ResponseEntity updateClient(Long id, ClientEntity clientEntity){
+        return clientRepository.findById(id)
+                .map(record -> {
+                    record.setName(clientEntity.getName());
+                    record.setAdress(clientEntity.getAdress());
+                    record.setCpf(clientEntity.getCpf());
+                    record.setRg(clientEntity.getRg());
+                    record.setSex(clientEntity.getSex());
+                    record.setEmail(clientEntity.getEmail());
+                    record.setTelephoneNumber(clientEntity.getTelephoneNumber());
+                    ClientEntity updated = clientRepository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+}
