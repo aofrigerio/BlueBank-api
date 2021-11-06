1e41a9d38ef782348f12b09ae9ab71664cbbea4e
Alan <aofrigerio@gmail.com>
2021-11-06 18:46:36 -0300

    Fim da task transação

--- src/main/java/br/com/codemasters/bluebank/services/TransactionService.java
+++ src/main/java/br/com/codemasters/bluebank/services/TransactionService.java
@@ -5,55 +5,52 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.codemasters.bluebank.domain.dtos.DepositDTO;
 import br.com.codemasters.bluebank.domain.dtos.DraftDTO;
 import br.com.codemasters.bluebank.domain.dtos.TransferDTO;
+import br.com.codemasters.bluebank.domain.entities.AccountEntity;
 import br.com.codemasters.bluebank.domain.entities.TransactionEntity;
-import br.com.codemasters.bluebank.domain.repository.ClientRepository;
 import br.com.codemasters.bluebank.domain.repository.TransactionRepository;
 
 @Service
 public class TransactionService {
 		
-	private final TransactionRepository transactionRepository;
+	private final TransactionRepository transactionRepository;	
+	private final AccountService accountService;
 	
 	@Autowired
-    public TransactionService(TransactionRepository transactionRepository){
+    public TransactionService(TransactionRepository transactionRepository, AccountService accountService ){
         this.transactionRepository = transactionRepository;
+        this.accountService = accountService;
     }
 	
 	@Transactional
 	public void deposit(DepositDTO depositDTO){
-		TransactionEntity entity = TransactionEntity.builder().account(null).value(depositDTO.getValue()).build();
+		AccountEntity accountEntity = accountService.findEntityById(depositDTO.getAccountId());		
+		TransactionEntity entity = TransactionEntity.builder().account(accountEntity).value(depositDTO.getValue()).build();			
+		accountService.depositUpdate(accountEntity.getId(), depositDTO.getValue());
 		transactionRepository.save(entity);
-		//TODO - Ajustar o saldo da conta
 	}
 	
 	@Transactional
-	public void draft(DraftDTO dto) {	
-		TransactionEntity entity = TransactionEntity.builder().account(null).value(dto.getValue()).build();
+	public void draft(DraftDTO draftDto) {			
+		AccountEntity accountEntity = accountService.findEntityById(draftDto.getAccountId());						
+		accountService.draftUpdate(accountEntity.getId(), draftDto.getValue());		
+		TransactionEntity entity = TransactionEntity.builder().account(accountEntity).value(draftDto.getValue()).build();
 		transactionRepository.save(entity);
-		//TODO - Ajustar o saldo
 	}
 	
 	@Transactional
-	public void transfer(TransferDTO transferDTO){
-		
-		//TODO - encontrar conta origem
-		//TODO - encontrar conta destino
-		//TODO - validar saldo origem
-		// SENÃO, LANÇAR UMA EXCEPTION
-		
-		TransactionEntity entityOrigin = TransactionEntity.builder().account(null).value(transferDTO.getValue()).build();
-		TransactionEntity entityDestiny = TransactionEntity.builder().account(null).value(transferDTO.getValue()).build();
-		//TODO - Ajustar o saldo conta origem
-		//TODO - Ajustar o saldo da conta destino
-		transactionRepository.saveAll(List.of(entityOrigin,entityDestiny));
-	 
+	public void transfer(TransferDTO transferDTO){		
+		AccountEntity accountOrigin = accountService.findEntityById(transferDTO.getAccountIdOrigin());		
+		AccountEntity accountDestiny = accountService.findEntityById(transferDTO.getAccountIdDestiny());
+		accountService.draftUpdate(accountOrigin.getId(), transferDTO.getValue());
+		accountService.depositUpdate(accountDestiny.getId(), transferDTO.getValue());
+		TransactionEntity entityOrigin = TransactionEntity.builder().account(accountOrigin).value(transferDTO.getValue()).build();
+		TransactionEntity entityDestiny = TransactionEntity.builder().account(accountDestiny).value(transferDTO.getValue()).build();
+		transactionRepository.saveAll(List.of(entityOrigin,entityDestiny));	
 	}
 	
 }
